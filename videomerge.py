1b9067536e92e889fac146d2299a9a8273714a23
Swastik Biswas <swastikbiswas962@gmail.com>
2023-05-05 20:46:31 +0530

    temp_fix

--- videomerge.py
+++ videomerge.py
@@ -13,23 +13,26 @@ if __name__ == "__main__":
         exit()
     if choice == 1:
         videos = []
+        txtFile = open("merge_data.txt", "w")
         for file in os.listdir("videos"):
             if file.endswith(".mp4"):
-                videos.append(VideoFileClip(os.path.join("videos", file)))
-        # write a json file merge_data.json with filename, start time and end time with respect to total merged video
-        with open("merge_data.json", "w") as f:
-            json.dump([], f)
-        with open("merge_data.json", "r") as f:
-            data = json.load(f)
-        total_time = 0
-        for i in range(0, len(videos)):
-            data.append({"filename": videos[i].filename, "start": total_time + videos[i].start, "end": total_time + videos[i].end})
-            total_time += videos[i].duration + 5
-        with open("merge_data.json", "w") as f:
-            json.dump(data, f)
-        final_video = concatenate_videoclips(videos)
-        final_video.write_videofile("video_merged.mp4")
-        print("Video merged successfully!")
+                txtFile.write("file '" + file + "'\n")
+        #         videos.append(VideoFileClip(os.path.join("videos", file)))
+        #         videos.append(VideoFileClip(os.path.join("videos", file)))
+        # # write a json file merge_data.json with filename, start time and end time with respect to total merged video
+        # with open("merge_data.json", "w") as f:
+        #     json.dump([], f)
+        # with open("merge_data.json", "r") as f:
+        #     data = json.load(f)
+        # total_time = 0
+        # for i in range(0, len(videos)):
+        #     data.append({"filename": videos[i].filename, "start": total_time + videos[i].start, "end": total_time + videos[i].end})
+        #     total_time += videos[i].duration + 5
+        # with open("merge_data.json", "w") as f:
+        #     json.dump(data, f)
+        # final_video = concatenate_videoclips(videos)
+        # final_video.write_videofile("video_merged.mp4")
+        # print("Video merged successfully!")
     if choice == 2:
         video = VideoFileClip("video_merged.mp4")
         with open("merge_data.json", "r") as f:
